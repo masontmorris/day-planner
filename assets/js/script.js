19060cf734596f5aa52c2a307595fad8c0e2123f
masontmorris <masonmorris8@gmail.com>
2022-03-20 01:57:48 -0400

    made entries persistent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,36 @@
 let saveBtn = $(".save-btn");
+let entryArray = [];
+onLoad();
+
 saveBtn.click(function () {
-    let textValue = $(this).parent().find("textarea").val();
+    let parentRow = $(this).parent();
+    let textValue = parentRow.find("textarea").val();
     console.log(textValue);
+
+    let newEntry = new Entry(textValue, parentRow.attr("id"));
+    console.log(newEntry);
+    let oldEntries = entryArray.filter((obj) => {
+        return obj.hour === newEntry.hour;
+    });
+    console.log(oldEntries);
+    entryArray = entryArray.filter((obj) => !oldEntries.includes(obj));
+    entryArray.push(newEntry);
+    localStorage.setItem("entryArray", JSON.stringify(entryArray));
+    console.log(entryArray);
 });
+
+function Entry(text, hour) {
+    this.text = text;
+    this.hour = hour;
+}
+
+function onLoad() {
+    entryArray = JSON.parse(localStorage.getItem("entryArray"));
+    entryArray = entryArray || [];
+    if (entryArray.length > 0) {
+        for (let i = 0; i < entryArray.length; i++) {
+            let textField = $("#" + entryArray[i].hour).find("textarea");
+            textField.val(textField.val() + entryArray[i].text + "\n");
+        }
+    }
+}
